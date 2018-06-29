8708ee9068bb1310cfdf9d41ae11d15488ceba0b
Josh Lester <joshl@bayardad.com>
2018-06-29 16:58:18 -0600

    removed react-hot-reload

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,5 @@
 // ./js/App.js
 import React from "react";
-import {hot} from "react-hot-loader";
 import "../scss/main.scss";
 
 const App = () => {
@@ -11,4 +10,4 @@ const App = () => {
   )
 }
 
-export default hot(module)(App)
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
     ]
   },
   plugins: [ 
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new CleanWebpackPlugin('dist', {} ),
